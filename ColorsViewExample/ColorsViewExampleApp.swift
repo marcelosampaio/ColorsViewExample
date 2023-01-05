bc51e7e45a84195d6b416868f2e6116fd0a9d93d
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2023-01-05 16:04:43 -0300

    Initial Commit

---
+++ ColorsViewExample/ColorsViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsViewExampleApp.swift
+//  ColorsViewExample
+//
+//  Created by Marcelo Sampaio on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorsViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
